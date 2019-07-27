1c9b3e8b764788f198264db6549d674d687b223e
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2019-07-27 09:31:53 +0700

    add scenario 1

--- src/theme.js
+++ src/theme.js
@@ -5,6 +5,20 @@ export default {
   styles: {
     code: {
       fontSize: '0.4em'
-    }
+    },
+    blockquote: {
+      background: '#f9f9f9',
+      borderLeft: '10px solid #ccc',
+      margin: '1.5em 10px',
+      padding: '0.5em 10px',
+      before: {
+        color: '#ccc',
+        content: 'open-quote',
+        fontSize: '4em',
+        lineHeight: '0.1em',
+        marginRight: '0.25em',
+        verticalAlign: '-0.4em',
+      }
+    },
   }
 }
